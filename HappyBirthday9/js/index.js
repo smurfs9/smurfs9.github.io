b804235e6778f0f049fae97aa21e339559ce9e68
Smurfs9 <smurfs3364@gmail.com>
2022-03-07 21:04:43 +0800

    Site updated: 2022-03-07 21:04:35

---
+++ HappyBirthday9/js/index.js
@@ -0,0 +1,14 @@
+$('#login-button').click(function (event) {
+	var userName=document.getElementById("userName").value;
+    var pwd=document.getElementById("pwd").value;
+		//修改密码请改此处
+    if(userName=="同桌的你" &&  pwd=="9999"){
+			event.preventDefault();
+			$('form').fadeOut(500);
+			$('.wrapper').addClass('form-success');
+			setTimeout(function(){location.href="BirthdayCake.html";},2000);
+		}
+	else{
+		alert("身份识别错误，请重新输入!");
+	}
+});
